5f915d152b66567937fd49b60c77f79dbb0d6c3b
Denis <Archakov.denisochka@gmail.com>
2026-03-01 21:56:14 +0300

    Добавление параметров в функцию

--- Book_MacGrat/def_func.py
+++ Book_MacGrat/def_func.py
@@ -1,12 +1,18 @@
-global_var =1
+# global_var =1
 
-def my_vars():
-    print('Глобальная переменная global_var\t', global_var)
-    local_var = 2
-    print('Локальная переменная  local_var\t', local_var)
-    global inner_var
-    inner_var = 3
+# def my_vars():
+#     print('Глобальная переменная global_var\t', global_var)
+#     local_var = 2
+#     print('Локальная переменная  local_var\t', local_var)
+#     global inner_var
+#     inner_var = 3
 
 
-my_vars()
-print('Вторая глобальная переменная global inner_var\t', inner_var)
+# my_vars()
+# print('Вторая глобальная переменная global inner_var\t', inner_var)
+
+# Добавление параметров в функцию.
+def echo(user, lang, sys):
+    print('\nПользователь: \t', user, '\nЯзык: \t\t', lang, '\nПлатформа: \t', sys)
+
+echo('Mike', 'Python', 'Windows')
