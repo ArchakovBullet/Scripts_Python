13f30dc00302d261f072f576dde0c6730732d7af
Denis <Archakov.denisochka@gmail.com>
2026-03-09 19:39:50 +0300

    from_import_func

---
+++ Book_MacGrat/dog.py
@@ -0,0 +1,9 @@
+def bark(pet ='Собака'):
+    print(pet,'говорит Гав_гав.')
+
+def lick(pet ='Собака'):
+    print(pet, 'пьет воду из лужи')
+
+def nap(pet = 'Собака'):
+    print(pet,'спит на солнышке')
+
---
+++ Book_MacGrat/new_dog_new.py
@@ -0,0 +1,7 @@
+from dog import*
+
+pet = input('Введите имя питомца -->  ')
+
+bark(pet)
+lick(pet)
+nap(pet)
