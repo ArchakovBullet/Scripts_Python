cdd8fe57f471b3b3b166bbb6c959ab8e568a2432
Denis <Archakov.denisochka@gmail.com>
2026-03-15 21:43:15 +0300

    Внес изменения в стих лермонтова - будет открываться мой вариант

--- Book_MacGrat/File_acceess_poem.py
+++ Book_MacGrat/File_acceess_poem.py
@@ -1,34 +1,34 @@
 # # Создан текст.
-# poem ='Скажи-ка, дядя, ведь недаром\n'
-# poem+='Москва, спаленная пожаром,\n'
-# poem+='Французу отдана?\n'
-# poem+='Ведь были ж схватки боевые,\n'
-# poem+='Да, говорят, еще какие!\n'
-# poem+='Недаром помнит вся Россия\n'
-# poem+='Про день Бородина!\n'
+poem ='Скажи-ка, дядя, ведь недаром\n'
+poem+='Москва, спаленная пожаром,\n'
+poem+='Французу отдана?\n'
+poem+='Ведь были ж схватки боевые,\n'
+poem+='Да, говорят, еще какие!\n'
+poem+='Недаром помнит вся Россия\n'
+poem+='Про день Бородина!\n'
 
-# # Создан файл и в него записан текст --> w
-# file = open('e:/Python/My_Scripts_Python/Book_MacGrat/stish_Lermontov.txt', 'w')
-# file.write(poem)
-# file.close()
+# Создан файл и в него записан текст --> w
+file = open('e:/Python/My_Scripts_Python/Book_MacGrat/stish_Lermontov.txt', 'w')
+file.write(poem)
+file.close()
 
-# # # Открыт файл для чтения --> r
-# # file = open('e:/Python/My_Scripts_Python/Book_MacGrat/stish_Lermontov.txt', 'r')
-# # # Выведен текст на экран.
-# # for line in file:
-# #     print(line, end='')
-# # file.close()
-
-# # В текстовый файл ДОБАВЛЕН автор произведения --> a
-# file = open('e:/Python/My_Scripts_Python/Book_MacGrat/stish_Lermontov.txt', 'a')
-# file.write('\n(Михаил Лермонтов.)', )
-# file.close()
-
-# # Теперь выводим все в печать.
+# # Открыт файл для чтения --> r
 # file = open('e:/Python/My_Scripts_Python/Book_MacGrat/stish_Lermontov.txt', 'r')
-# for line_avtor  in file:
-#     print(line_avtor, end=' ')
+# # Выведен текст на экран.
+# for line in file:
+#     print(line, end='')
 # file.close()
+
+# В текстовый файл ДОБАВЛЕН автор произведения --> a
+file = open('e:/Python/My_Scripts_Python/Book_MacGrat/stish_Lermontov.txt', 'a')
+file.write('  (Михаил Лермонтов.)', )
+file.close()
+
+# Теперь выводим все в печать.
+file = open('e:/Python/My_Scripts_Python/Book_MacGrat/stish_Lermontov.txt', 'r')
+for line_avtor  in file:
+    print(line_avtor, end=' ')
+file.close()
 
 # Код от Deep Seek
 # Создан текст.
@@ -71,30 +71,30 @@
 # Еще более усовершенствованный код, исключающий ошибки, от Deep Seek
 # Этот способ гарантирует закрытие файла даже в случае ошибок.
 # Создан текст.
-poem = 'Скажи-ка, дядя, ведь недаром\n'
-poem += 'Москва, спаленная пожаром,\n'
-poem += 'Французу отдана?\n'
-poem += 'Ведь были ж схватки боевые,\n'
-poem += 'Да, говорят, еще какие!\n'
-poem += 'Недаром помнит вся Россия\n'
-poem += 'Про день Бородина!'
+# poem = 'Скажи-ка, дядя, ведь недаром\n'
+# poem += 'Москва, спаленная пожаром,\n'
+# poem += 'Французу отдана?\n'
+# poem += 'Ведь были ж схватки боевые,\n'
+# poem += 'Да, говорят, еще какие!\n'
+# poem += 'Недаром помнит вся Россия\n'
+# poem += 'Про день Бородина!'
 
-filename ='e:/Python/My_Scripts_Python/Book_MacGrat/stish_Lermontov_Great.txt'
+# filename ='e:/Python/My_Scripts_Python/Book_MacGrat/stish_Lermontov_Great.txt'
 
-# Запись
-with open(filename, 'w', encoding='utf-8') as file:
-    file.write(poem)
+# # Запись
+# with open(filename, 'w', encoding='utf-8') as file:
+#     file.write(poem)
 
-# Чтение
-with open(filename, 'r', encoding='utf-8') as file:
-    for line in file:
-        print(line, end='')
+# # Чтение
+# with open(filename, 'r', encoding='utf-8') as file:
+#     for line in file:
+#         print(line, end='')
 
-# Добавление
-with open(filename, 'a', encoding='utf-8') as file:
-    file.write('\n(Михаил Лермонтов.)')
+# # Добавление
+# with open(filename, 'a', encoding='utf-8') as file:
+#     file.write('\n(Михаил Лермонтов.)')
 
-# Проверка
-with open(filename, 'r', encoding='utf-8') as file:
-    print("\n\nФинальный текст:")
-    print(file.read())
+# # Проверка
+# with open(filename, 'r', encoding='utf-8') as file:
+#     print("\n\nФинальный текст:")
+#     print(file.read())
