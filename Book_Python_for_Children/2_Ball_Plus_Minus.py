e6c8b6f9f17c603ee56dfc1d4b056c113e20a7c4
Denis <Archakov.denisochka@gmail.com>
2026-04-19 21:20:09 +0300

    Работа с шариком

---
+++ Book_Python_for_Children/2_Ball_Plus_Minus.py
@@ -0,0 +1,80 @@
+''''''' Вторая программа "Шарик за курсором, стр. 47 книги." '''
+
+import pygame as pg, sys
+import os
+
+pg.init()
+
+
+# Перемещаем окно на второй монитор (если он есть)
+# Расположение окна при запуске программы на втором мониторе (справа)
+# Эта переменная ниже должна быть создана до pg.display.set_mode( )
+os.environ['SDL_VIDEO_WINDOW_POS'] = " 2480,240 "
+
+# Размер окошка:
+screen = pg.display.set_mode((800, 600))
+
+# Если есть желание открыть монитор в полноэкранном режиме,
+# но для выхода из программы нужно сделать обработку.Ниже смотри: Выход при нажатии на Escape.
+# screen = pg.display.set_mode((0, 0), pg.FULLSCREEN, display=1)
+
+# надпись на окошке слева вверху:
+pg.display.set_caption('Лети к курсору')
+
+print("\n" + "#" * 50)
+print("#" * 50 + '\n')
+print("Для выхода из игры нажмите Escape.")
+print('\nПробел - смена фона на синий')
+print('\nЦифра 1 = Серый цвет\n')
+print('Цифра 2 = Зеленый цвет \n')
+print('Цифра 3 = Белый цвет\n')
+print('+ = Увеличить размер шарика.')
+print('- = Уменьшить размер шарика.')
+print('\n' + "#" * 50)
+print("#" * 50)
+
+#  начальные координаты объекта(шарика) в окошке при запуске.
+x,y= 400,300
+size = 60 #размер шарика. Можно например 200 бахнуть.
+color = (255,0,0) # цвет красный. Если зеленый то: (0, 255, 0)
+# color = (0,255,0)   # зеленый то: (0, 255, 0)
+
+
+clock = pg.time.Clock()  # альтернатива  pg.time.delay(10) 
+
+# По сути, это ДВИЖОК ПРОГРАММЫ.
+while True: # Бесконечный игровой цикл. 
+    for event in pg.event.get():  # Получаем накопившиеся события.
+        if event.type == pg.QUIT: # Нажал на крестик ?
+            pg.quit()
+            sys.exit()
+        elif event.type == pg.KEYDOWN: # Обрабатываем нажатие клавиш
+            if event.key == pg.K_ESCAPE: # ESCAPE
+                pg.quit()
+                sys.exit()
+        elif event.type == pg.MOUSEBUTTONDOWN: # Нажал кнопку мыши ?
+            x,y = pg.mouse.get_pos()  # Получаем координаты мыши.
+
+    # screen.fill((0,0,0))   # Закрашивает экран черным цветом
+
+    keys = pg.key.get_pressed()
+    if keys[pg.K_SPACE]: # Если нажал на пробел,
+        screen.fill((0,0,255))  # то, окно станет синим.
+
+    if keys[pg.K_KP_PLUS]: 
+        size += 1
+    if keys[pg.K_KP_MINUS]: 
+        size -= 1
+    
+    if keys[pg.K_1]:
+        color = (50,100,100)
+    if keys[pg.K_2]:
+        color = (0,255,0)
+    if keys[pg.K_3]:
+        color = (255,255,255)
+       
+
+    pg.draw.circle(screen, color, (x, y), size)
+    pg.display.flip()
+    # pg.time.delay(10)  # задержка на 10 милисекунд, чтобы не перегревался процессор. Этой строке есть альтернатива.
+    clock.tick(60)
