53c5fed1f7631bee2f2ec7e8ee9107373ef41163
Denis <Archakov.denisochka@gmail.com>
2026-05-01 23:52:52 +0300

    Балуюсь с кодом шарика

--- Book_Python_for_Children/3_Ball_New_Interface.py
+++ Book_Python_for_Children/3_Ball_New_Interface.py
@@ -1,4 +1,4 @@
-'''''''Новый интерфейс старого кода(стр.50) '''''''
+'''''''Новый интерфейс старого кода(стр.50) - 01.05.2026'''''''
 
 import pygame as pg, sys
 import os
@@ -10,7 +10,7 @@ screen = pg.display.set_mode((800, 600))
 pg.display.set_caption('Взлетаем к курсору')
 
 print('\n' +'#' * 50)
-print('Для выхода из игры нажмите Escape\nлибо нажмите крестик.\n')
+print('Для выхода из игры нажмите Escape\nлибо нажмите крестик.\nSpace для очистки от следов')
 print('\n' +'#' * 50)
 
 # Небесно-голубой
@@ -39,7 +39,19 @@ x,y = 400,300
 size = 60
 color = cherry
 
-font = pg.font.SysFont('Arial', 24)
+font = pg.font.SysFont('Arial', 24) # шрифт для надписи Размер.
+
+'''''''''
+# Системные шрифты (доступны везде)
+font = pg.font.SysFont('Arial', 24)        # классический
+font = pg.font.SysFont('Comic Sans MS', 20) # игривый
+font = pg.font.SysFont('Times New Roman', 28) # строгий
+font = pg.font.SysFont('Courier New', 16)  # как в печатной машинке
+font = pg.font.SysFont('Verdana', 24)      # хорош для интерфейсов
+
+# Шрифт по умолчанию (если не нашел указанный)
+font = pg.font.SysFont(None, 24)           # любой системный
+'''''
 
 clock = pg.time.Clock()
 
@@ -63,7 +75,7 @@ while True:
     if keys[pg.K_2]: color = lemon
     if keys[pg.K_3]: color = green_color
 
-    if keys[pg.K_SPACE]: screen.fill(sky_blue)
+    if keys[pg.K_SPACE]: screen.fill(black)
 
     if 15 < size < 100:
         if keys[pg.K_KP_MINUS]: size -= 1
@@ -73,7 +85,19 @@ while True:
     pg.draw.rect(screen, (50,50,50), (0,550,800, 50))
     pg.draw.circle(screen,color, (50,575), 15)
     pg.draw.circle(screen,(200, 200,200), (50, 575), 15, 1 )
-    size_text = font.render(f'Размер: {size} ', True, (255,255,255))
+    size_text = font.render(f'Размер: {size} ', True, (255,255,0))
+
+    # Примеры цветов:
+    # Белый текст
+    white_text = font.render('Белый текст', True, (255, 255, 255))
+
+    # Желтый текст
+    yellow_text = font.render('Желтый текст', True, (255, 255, 0))
+
+    # Красный текст с черным фоном
+    red_text = font.render('Красный текст', True, (255, 0, 0), (0, 0, 0))
+    
+
     screen.blit(size_text, (80,560))
 
     pg.display.flip()
