009e21d386728106e19c4c5a54bb38c705c6f5e7
Denis <Archakov.denisochka@gmail.com>
2026-05-08 12:32:08 +0300

    Ищу понимания функции

---
+++ Book_MacGrat/Test_function_My.py
@@ -0,0 +1,10 @@
+'''''''Хочу понимать вызов функций'''''''
+
+def meeting(first, second):
+    print(f'{first} встречает {second}')
+
+
+first = input('Введите имя первого героя  ')
+second = input('Введите имя второго героя ')
+
+meeting(first, second)
